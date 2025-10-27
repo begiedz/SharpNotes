b3b40a90aa8f22448be2008fe4b3651f9f51625b
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-10-27 15:46:42 +0100

    feat: add Note controller

---
+++ SharpNotes.Api/Controllers/NoteController.cs
@@ -0,0 +1,18 @@
+﻿using SharpNotes.Domain.Entities;
+using SharpNotes.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharpNotes.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/all")]
+public class NoteController : ControllerBase
+{
+    public NoteController()
+    {
+    }
+
+    [HttpGet]
+    public ActionResult<List<Note>> GetAll() =>
+    NoteService.GetAll();
+}
