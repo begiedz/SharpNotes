0db2daf5e7224549e5fcdb42dd25348f007c9878
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-10-27 15:45:52 +0100

    feat: add Note entity

---
+++ SharpNotes.Domain/Entities/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpNotes.Domain.Entities;
+
+public class Note
+{
+    public Guid ID { get; set; }
+    public string Title { get; set; } = String.Empty;
+    public string Content { get; set; } = String.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
