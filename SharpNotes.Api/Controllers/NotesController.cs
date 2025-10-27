9c38dd0abd979f26e7c72eb1152f82225866adf6
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-10-27 19:10:30 +0100

    refactor: endpoint convenction to /api/

--- SharpNotes.Api/Controllers/NotesController.cs
+++ SharpNotes.Api/Controllers/NotesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SharpNotes.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class NotesController : ControllerBase
 {
     public NotesController()
