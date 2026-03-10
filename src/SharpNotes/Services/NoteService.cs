798ca8ced3e4def1b349beb606b90a64f9088219
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-10 10:39:20 +0100

    feat: add get by id definition

--- src/SharpNotes/Services/NoteService.cs
+++ src/SharpNotes/Services/NoteService.cs
@@ -8,9 +8,14 @@ public class NoteService(AppDbContext context) : INoteService
 {
     private readonly AppDbContext _context = context;
 
-    public async Task<List<Note>> GetNotesAsync()
+    public async Task<List<Note>> GetAllAsync()
     {
         var notes = await _context.Notes.AsNoTracking().ToListAsync();
         return notes;
+    }
+    public async Task<Note?> GetByIdAsync(int id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        return note;
     }
 }
